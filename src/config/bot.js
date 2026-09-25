00b0ea81ee8fdb3362909c08d6841884db4e04d3
bt00kiby <kibiiry11@gmail.com>
2026-09-25 14:09:38 +0700

    Update bot status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "𝐘𝐞𝐮𝐡 𝐊𝐞𝐩𝐨 𝐋𝐮 𝐌𝐩𝐮𝐲",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
